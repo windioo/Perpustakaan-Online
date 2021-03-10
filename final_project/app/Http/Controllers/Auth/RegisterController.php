db5c9ad82b5f1891b0f276915d09d6e1ead366db
azrianasari <azrianasari867@gmail.com>
2021-03-11 06:20:38 +0800

    minor register delete dd

--- final_project/app/Http/Controllers/Auth/RegisterController.php
+++ final_project/app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        dd($data);
+        
         $newuser = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
